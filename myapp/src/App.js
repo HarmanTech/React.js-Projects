a8295410b7eee63e6098c45cdb2dcbf628c1642f
HarmanTech <61256121+HarmanTech@users.noreply.github.com>
2021-05-22 18:18:32 -0400

    First project
    using 3 components

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import Heading from "./Heading";
+import Introduction from "./Introduction";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Heading />
+      <Introduction />
+      <Footer />
+   </>
+  )
+}
+
+
+export default App;
---
+++ myapp/src/Introduction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Introduction() {
+    return (
+        <div>
+            <p> I am a front end web developer</p>
+        </div>
+    )
+}
